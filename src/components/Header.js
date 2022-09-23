b40be1365097f3b6856b947f81e2735e9ae0a08f
KimSeYoung <sagekim6@gmail.com>
2022-09-23 22:17:50 +0900

    Home background Image 반응형

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
 import Logo from "../assets/shared/logo.svg";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
+import { useEffect } from "react";
 
 const Header = () => {
+  const location = useLocation();
+
+  const handleBackgroundChange = () => {
+    if (location.pathname === "/destination/") {
+      console.log("/destination/");
+    } else if (location.pathname === "/crew/") {
+      console.log("/crew/");
+    } else if (location.pathname === "/technology/") {
+      console.log("/technology/");
+    } else if (location.pathname === "/") {
+      console.log("/");
+    }
+  };
+
+  useEffect(() => {
+    handleBackgroundChange();
+  }, [location]);
+
   return (
     <header className="Header">
       <div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,12 @@ import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div className="Home">
+    <>
       <Header />
-      <h1>This is Home</h1>
-    </div>
+      <div className="Home">
+        <h1>This is Home</h1>
+      </div>
+    </>
   );
 };
 
